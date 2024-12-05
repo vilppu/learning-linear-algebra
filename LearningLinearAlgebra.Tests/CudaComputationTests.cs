a123c7f58f66b355d5cc618292caed7b4045bf79
vilppu <tsvilp@gmail.com>
2024-12-06 00:17:18 +0200

    Cuda computation

---
+++ LearningLinearAlgebra.Tests/CudaComputationTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LearningLinearAlgebra.Tests;
+
+public class CudaComputationTests
+{
+    [Fact]
+    public void Compute_on_NVIDIA_GPU()
+    {
+        var result = CudaComputation.ComputeInGpu();
+
+        result.Should().Be(11);
+    }
+}
---
+++ LearningLinearAlgebra/CudaComputation.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LearningLinearAlgebra
+{
+    public static partial class CudaComputation
+    {
+
+        public static int ComputeInGpu()
+        {
+            return Compute();
+        }
+
+
+        [LibraryImport("CudaComputation.dll", StringMarshalling = StringMarshalling.Utf16, SetLastError = true)]
+        private static partial int Compute();
+    }
+}
